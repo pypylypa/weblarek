5e8ba94fa94b5a0b158ec5766c324355211ad84a
Dima Krichevskiy <satana222239@gmail.com>
2025-11-04 08:51:44 +0300

    ПР9.1

--- src/components/view/Basket.ts
+++ src/components/view/Basket.ts
@@ -1,42 +1,70 @@
-import { ensureElement } from "../../utils/utils"
-import { EventEmitter } from "../base/Events"  
-import { Component } from "../base/Component"   
+import { ensureElement } from "../../utils/utils";
+import { EventEmitter } from "../base/Events";
+import { Component } from "../base/Component";
 
 interface IBasketData {
   items: HTMLElement[];
   total: number;
 }
 
-export class Basket extends Component<IBasketData> { 
+export class Basket extends Component<IBasketData> {
   protected basketListElement: HTMLElement;
   protected basketPriceElement: HTMLElement;
   protected basketTitleElement: HTMLElement;
-  protected basketButtonOrderElement: HTMLButtonElement; 
- 
-  constructor(container: HTMLElement, protected events: EventEmitter) { 
-    super(container) 
-    this.basketButtonOrderElement = ensureElement<HTMLButtonElement>('.basket__button', this.container);
-    this.basketTitleElement = ensureElement<HTMLElement>('.modal__title', this.container); 
-    this.basketPriceElement = ensureElement<HTMLElement>('.basket__price', this.container)
-    this.basketListElement = ensureElement<HTMLElement>('.basket__list', this.container);
-    this.basketButtonOrderElement.addEventListener('click', () => { 
-    this.events.emit('basket:order'); 
-    }); 
-  } 
+  protected basketButtonOrderElement: HTMLButtonElement;
 
-  set items(value: HTMLElement[]) {  
-    if (value.length === 0) {  
-      this.basketListElement.innerHTML = 'Корзина пуста';  
-      this.basketListElement.classList.add('basket__list-disabled');
-      this.basketButtonOrderElement.disabled = true;  
-    } else {  
-      this.basketListElement.replaceChildren(...value);  
-      this.basketListElement.classList.remove('basket__list-disabled');
-      this.basketButtonOrderElement.disabled = false;  
-    }  
+  constructor(container: HTMLElement, protected events: EventEmitter) {
+    super(container);
+
+    this.basketButtonOrderElement = ensureElement<HTMLButtonElement>(
+      '.basket__button',
+      this.container
+    );
+    this.basketTitleElement = ensureElement<HTMLElement>(
+      '.modal__title',
+      this.container
+    );
+    this.basketPriceElement = ensureElement<HTMLElement>(
+      '.basket__price',
+      this.container
+    );
+    this.basketListElement = ensureElement<HTMLElement>(
+      '.basket__list',
+      this.container
+    );
+
+    // Устанавливаем начальное состояние (пустая корзина)
+    this.items = [];
+
+    // Обработчик клика по кнопке заказа
+    this.basketButtonOrderElement.addEventListener('click', () => {
+      this.events.emit('basket:order');
+    });
   }
 
-  set total(value: number) {  
-    this.basketPriceElement.textContent = `${value} синапсов`;  
-  }  
-} 
+  /**
+   * Сеттер для управления списком товаров в корзине
+   * @param value - массив HTMLElement или null/undefined
+   */
+  set items(value: HTMLElement[] | undefined | null) {
+    if (!value || value.length === 0) {
+      // Корзина пуста
+      this.basketListElement.innerHTML = 'Корзина пуста';
+      this.basketListElement.classList.add('basket__list-disabled');
+      this.basketButtonOrderElement.disabled = true;
+    } else {
+      // Есть товары в корзине
+      this.basketListElement.replaceChildren(...value);
+      this.basketListElement.classList.remove('basket__list-disabled');
+      this.basketButtonOrderElement.disabled = false;
+    }
+  }
+
+  /**
+   * Сеттер для отображения общей стоимости
+   * @param value - числовое значение суммы
+   */
+  set total(value: number) {
+    this.basketPriceElement.textContent = `${value} синапсов`;
+  }
+}
